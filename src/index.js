c7cdb796903a32ec114465c0bb134f6e45dcd973
abhinavreddydev16 <abhinavreddybokkadev@gmail.com>
2026-01-27 23:09:19 +0530

    my first commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+
+import config from './config/index.js';
+
+const app = express();
+
+app.listen(config.PORT , () => { console.log(`server is running on the ${PORT}`)});
+
